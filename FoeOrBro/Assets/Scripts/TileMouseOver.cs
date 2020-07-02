ef95f642c26ad7a5c82c25b21627f9c3afbb605d
SigmundurJohansen <nebuchanezher@hotmail.com>
2020-07-02 21:53:21 +0100

    map generator framework 0.1

---
+++ FoeOrBro/Assets/Scripts/TileMouseOver.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileMouseOver : MonoBehaviour
+{
+    public CameraController highLightColor;
+    Color normalColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ FoeOrBro/Assets/Scripts/map/MapGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public Transform tilePrefab;
+    public Vector2 mapSize;
+
+    public void GenerateMap(){
+        for (int x = 0; x < mapSize.x; x++){
+            for(int y = 0; y < mapSize.y; y++){
+                //Vector3 tilePosition = new Vector3(-)
+            }
+        }
+    }
+
+}
