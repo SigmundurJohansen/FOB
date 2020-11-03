f5fe3ed2b1cd4f8980b656a254a6737c41dd9bfd
SigmundurJohansen <nebuchanezher@hotmail.com>
2020-11-03 00:19:40 +0000

    expanded GUI, added destination target delay

--- FoeOrBro/Assets/Scripts/game_mechanics/systems/DestinationSystem.cs
+++ FoeOrBro/Assets/Scripts/game_mechanics/systems/DestinationSystem.cs
@@ -12,20 +12,27 @@ using UnityEngine.SceneManagement;
 
 public class DestinationSystem : ComponentSystem
 {
+    float targetDelay = 0.5f;
+
     protected override void OnUpdate()
     {
-        Entities.WithNone<DestinationComponent>().ForEach((Entity entity, ref Translation translation, ref HasTarget hasTarget,ref MovementComponent _move) =>
+        targetDelay -= UnityEngine.Time.deltaTime;
+        if (targetDelay < 0)
         {
-            if (World.DefaultGameObjectInjectionWorld.EntityManager.Exists(hasTarget.targetEntity))
+            Entities.WithNone<DestinationComponent>().ForEach((Entity entity, ref Translation translation, ref HasTarget hasTarget, ref MovementComponent _move) =>
             {
-                Translation targetTranslation = World.DefaultGameObjectInjectionWorld.EntityManager.GetComponentData<Translation>(hasTarget.targetEntity);
-                PathfindingGridSetup.Instance.pathfindingGrid.GetXY(translation.Value, out int endX, out int endY);
-                PathfindingGridSetup.Instance.pathfindingGrid.GetXY(targetTranslation.Value, out int startX, out int startY);
-                World.DefaultGameObjectInjectionWorld.EntityManager.AddComponentData(entity, new DestinationComponent { startPosition = new int2(startX, startY), endPosition = new int2(endX, endY) });
-                _move.isMoving = true;
-                Debug.Log("destination given");
-            }
-        });
+                if (World.DefaultGameObjectInjectionWorld.EntityManager.Exists(hasTarget.targetEntity))
+                {
+                    Translation targetTranslation = World.DefaultGameObjectInjectionWorld.EntityManager.GetComponentData<Translation>(hasTarget.targetEntity);
+                    PathfindingGridSetup.Instance.pathfindingGrid.GetXY(translation.Value, out int startX, out int startY);
+                    PathfindingGridSetup.Instance.pathfindingGrid.GetXY(targetTranslation.Value, out int endX, out int endY);
+                    World.DefaultGameObjectInjectionWorld.EntityManager.AddComponentData(entity, new DestinationComponent { startPosition = new int2(startX, startY), endPosition = new int2(endX, endY) });
+                    _move.isMoving = true;
+                    Debug.Log("destination given");
+                }
+            });
+            targetDelay = 0.5f;
+        }
     }
 
 }
